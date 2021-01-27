741bf7f4304e20af41151ec4e4448ec916098735
stevenJoyce <37178649+stevenJoyce@users.noreply.github.com>
2021-01-27 20:34:45 +0000

    Created new pages for app and added MongoDB realm

--- app/src/main/java/com/example/sportsadvisor/MainActivity.kt
+++ app/src/main/java/com/example/sportsadvisor/MainActivity.kt
@@ -4,12 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import okhttp3.*
 import java.io.IOException
+import io.realm.Realm
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        Realm.init(this) // context, usually an Activity or Application
         //val longitude = 53.2707;
         // val latitude = 9.0568;
         // val result = URL("https://metwdb-openaccess.ichec.ie/metno-wdb2ts/locationforecast?lat=<53.2707>;long=<9.0568>").readText()
---
+++ app/src/main/java/com/example/sportsadvisor/UserHistory.kt
@@ -0,0 +1,11 @@
+package com.example.sportsadvisor
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class UserHistory : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
