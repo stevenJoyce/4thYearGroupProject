f76c6fd3d283ba72ddcaf60043e77d20cd23b45c
EvanGreaney <g00348451@gmit.ie>
2021-03-31 18:46:39 +0100

    Processing data now complete, awaiting parsing of data

--- app/src/main/java/com/example/sportsadvisor/UserResults.kt
+++ app/src/main/java/com/example/sportsadvisor/UserResults.kt
@@ -1,34 +1,21 @@
 package com.example.sportsadvisor
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import okhttp3.*
-import okhttp3.Request;
-import okhttp3.Response
-import java.io.IOException
-import okhttp3.MediaType
-import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.OkHttpClient
-import okhttp3.RequestBody
-import okhttp3.RequestBody.Companion.create
-
-
 open class UserResults  {
 
     var rainfall:Double = 0.0
-    var windSpeed:Double = 22.2
-    var temperature:Double = 14.1
-    var visibilty:Double = 16.1
-    var humidity:Int = 64
+    var windSpeed:Double = 9.3
+    var temperature:Double = 10.0
+    var feelsLikeTemp:Double = 8.9
+    var humidity:Int = 86
 
-    open fun checkResults(rainfall:Double,windSpeed:Double,temperature:Double,visibilty:Double,humidity:Int): Double {
+    open fun checkResults(rainfall:Double,windSpeed:Double,temperature:Double,feelsLikeTemp:Double,humidity:Int): Double {
         val rainResult = checkRainfall(rainfall)
         val windResult = checkWindSpeed(windSpeed)
         val tempResult = checkTemp(temperature)
-        val visibiltyResult = checkVisbility(visibilty)
+        val tempFeelResult = checkTempFeel(feelsLikeTemp)
         val humidityResult = checkHumidity(humidity)
 
-        val TotalResultScore = humidityResult + windResult+ tempResult + visibiltyResult + rainResult
+        val TotalResultScore = humidityResult + windResult+ tempResult + tempFeelResult + rainResult
 
         return TotalResultScore
     }
@@ -45,16 +32,13 @@ open class UserResults  {
             when
             {
                 ((humidity <= 10) or (humidity >=90)) ->{
-                    result = 0.2
+                    result = 0.3
                 }
-                ((humidity > 10) or(humidity <= 20))  ->{
-                    result = 0.4
+                ((humidity > 10) or(humidity <= 29))  ->{
+                    result = 0.5
                 }
-                humidity < 0.40 ->{
-                    result = 0.6
-                }
-                humidity > 90 ->{
-                    result = 0.2
+                ((humidity > 60) or (humidity < 90)) -> {
+                    result = 0.7
                 }
             }
         }
@@ -62,31 +46,136 @@ open class UserResults  {
         return result
     }
 
-    private fun checkVisbility(visibilty: Double): Double {
-        val result = 0.0;
+    private fun checkTempFeel(feelsLikeTemp: Double): Double {
+        var result = 0.0;
+
+        if (feelsLikeTemp in 10.0..20.0)
+        {
+            result = 1.0;
+        }
+        else
+        {
+            when
+            {
+                ((feelsLikeTemp > 20.0) and (feelsLikeTemp <=25.0)) ->{
+                    result = 0.8
+                }
+                ((feelsLikeTemp  >25.0) and (feelsLikeTemp <= 30.0))  ->{
+                    result = 0.6
+                }
+                ((feelsLikeTemp > 5.0) and (feelsLikeTemp < 10.0)) -> {
+                    result = 0.4
+                }
+                ((feelsLikeTemp > 30.0) and (feelsLikeTemp <= 35.0)) -> {
+                    result = 0.2
+                }
+                ((feelsLikeTemp > 0.0) and (feelsLikeTemp <= 5.0)) -> {
+                    result = 0.2
+                }
+                ((feelsLikeTemp > 35.0) or (feelsLikeTemp <= 0))  -> {
+                    result = 0.0
+                }
+            }
+        }
         return result
     }
 
     private fun checkTemp(temperature: Double): Double {
-        val result = 1.0;
+        var result = 0.0;
+
+        if (temperature in 10.0..20.0)
+        {
+            result = 1.0;
+        }
+        else
+        {
+            when
+            {
+                ((temperature > 20.0) and (temperature <=25.0)) ->{
+                    result = 0.8
+                }
+                ((temperature  >25.0) and (temperature <= 30.0))  ->{
+                    result = 0.6
+                }
+                ((temperature > 5.0) and (temperature < 10.0)) -> {
+                    result = 0.4
+                }
+                ((temperature > 30.0) and (temperature <= 35.0)) -> {
+                    result = 0.2
+                }
+                ((temperature > 0.0) and (temperature <= 5.0)) -> {
+                    result = 0.2
+                }
+                ((temperature > 35.0) or (temperature <= 0))  -> {
+                    result = 0.0
+                }
+            }
+        }
         return result
     }
 
     private fun checkRainfall(rainfall: Double): Double {
-        val result = 1.0;
+        var result = 0.0;
+
+        if (rainfall in 0.0..1.0)
+        {
+            result = 1.0;
+        }
+        else
+        {
+            when
+            {
+                (rainfall < 2.0) ->{
+                    result = 0.8
+                }
+                (rainfall < 3.0)  ->{
+                    result = 0.6
+                }
+                (rainfall < 4.0) -> {
+                    result = 0.4
+                }
+                (rainfall < 5.0) -> {
+                    result = 0.2
+                }
+                (rainfall > 5.0)  -> {
+                    result = 0.0
+                }
+            }
+        }
+
         return result
     }
+
 
     private fun checkWindSpeed(windSpeed: Double): Double {
+        var result = 0.0;
 
-        val result = 0.0;
+        if (windSpeed in 0.0..8.0)
+        {
+            result = 1.0;
+        }
+        else
+        {
+            when
+            {
+                ((windSpeed >= 9.0) and (windSpeed <=16.0)) ->{
+                    result = 0.8
+                }
+                ((windSpeed >= 17.0) and (windSpeed <= 24.0))  ->{
+                    result = 0.6
+                }
+                ((windSpeed >= 25.0) and (windSpeed <= 32.0)) -> {
+                    result = 0.4
+                }
+                ((windSpeed >= 33.0) and (windSpeed <= 40.0)) -> {
+                    result = 0.2
+                }
+                (windSpeed >= 40.0)  -> {
+                    result = 0.0
+                }
+            }
+        }
+
         return result
     }
-
-
-
-
-
-
-
 }
