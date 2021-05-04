75a6a5f501c9a448c015afe68a0995976abab6d6
stevenJoyce <37178649+stevenJoyce@users.noreply.github.com>
2021-05-04 12:03:20 +0100

    Changes key to UserID, changed Chapter 5 - MongoDB section

--- app/src/main/java/com/example/sportsadvisor/UserHistory.kt
+++ app/src/main/java/com/example/sportsadvisor/UserHistory.kt
@@ -1,8 +1,10 @@
 package com.example.sportsadvisor
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceManager
@@ -12,6 +14,7 @@ import io.realm.mongodb.User
 import io.realm.mongodb.mongo.iterable.MongoCursor
 import org.bson.Document
 
+
 class UserHistory : AppCompatActivity() {
     lateinit var app: App
     var list =  mutableListOf<String>()
@@ -19,43 +22,34 @@ class UserHistory : AppCompatActivity() {
     lateinit var results: MongoCursor<Document>
     lateinit var colResults:String
     private lateinit var text: TextView
+
+    //variables used to store the MongoDB data into a string
     var listString:String=""
     var fl:String = ""
     var fl2:String = ""
     var fl3:String = ""
     var fl4:String = ""
-    var pkey:String = ""
+    var userID:String = ""
     var user: User? = null
+    //Allow the data sent back to not include the ObjectID
     var query = Document("_id",false)
-    var field1 = Document("date",true)
-    var field2 = Document("time",true)
-    var field3 = Document("Course",true)
-    var field4 = Document("parScore",true)
-    var field5 = Document("handicap",true)
-    var field6= Document("totalScore",true)
-    var field7 = Document("_key",false)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.userhistory_main)
+
+        //The MongoDB Realm App ID
         val appID = "sportsadvisor-gztkm"
         app = App(AppConfiguration.Builder(appID).build())
         text = findViewById(R.id.userHistory)
         val sp = PreferenceManager.getDefaultSharedPreferences(applicationContext)
         val displayName = sp.getString("displayName", "")
-        pkey = displayName.toString()
-
+        userID = displayName.toString()
         user = app.currentUser()
-       /* if(app.currentUser() == null)
-        {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            showToast("ERROR: need to login to the application")
-        }
-*/
 
     }
-    fun userData(view: View) {
+    fun userData(view:View) {
         val mongoClient =
             user!!.getMongoClient("mongodb-atlas") // service for MongoDB Atlas cluster containing custom user data
         val mongoDatabase = mongoClient.getDatabase("Users")
@@ -64,7 +58,7 @@ class UserHistory : AppCompatActivity() {
         Log.v("EXAMPLE", "Successfully instantiated the MongoDB collection handle")
 
         //getting a cluster
-        val queryFilter = Document("_pkey", pkey)
+        val queryFilter = Document("userID", userID)
         val findTask = mongoCollection.find(queryFilter).projection(query).iterator()
         findTask.getAsync { task ->
             if (task.isSuccess) {
@@ -73,34 +67,28 @@ class UserHistory : AppCompatActivity() {
                 results = task.get()
                 Log.v("EXAMPLE", "successfully found all collections:")
                 var x = 0
-                    var colCount = 1
-                    while (results.hasNext()) {
-                        colResults = results.next().toString()
-                        //Log.v("First colResults: ", colResults)
-                        filteredList.add(colResults.split("[","{{",",","}}","Document{{","]").toString())
-                        fl += " Collection " + colCount + "\n" + filteredList[x]  + "\n\n"
-                        fl2 = fl.replace("[","")
-                        fl3 = fl2.replace(",","\n")
-                        fl4 = fl3.replace("]","")
-                        listString = fl4
-                        x++
-                        colCount++
-                        //Log.v("EXAMPLE", listString)
-                    }
-
-                Log.v("List", listString)
-
+                var colCount = 1
+                while (results.hasNext()) {
+                    colResults = results.next().toString()
+                    filteredList.add(colResults.split("[","{{",",","}}","Document{{","]").toString())
+                    fl += " Collection " + colCount + "\n" + filteredList[x]  + "\n\n"
+                    fl2 = fl.replace("[","")
+                    fl3 = fl2.replace(",","\n")
+                    fl4 = fl3.replace("]","")
+                    listString = fl4
+                    x++
+                    colCount++
+                    Log.v("List", listString)
+                }
             } else {
                 Log.e("EXAMPLE", "failed to find documents with: ${task.error}")
+                listString = " No User data found"
             }
            // println("List ${listString}")
 
-
         }
-
-        text.text = listString
-
-
+        //send stored data to page
+        text.setText(listString)
 
     }
 
